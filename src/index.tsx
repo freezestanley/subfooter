dd9a99c893ae8bf1a8704189f0fac7fa1f93f763
徐翔 <xuxiang001@zhongan.com>
2022-12-04 22:38:21 +0800

    docs: 12313

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
  * @Author:
  * @Data: Do not edit
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-12-04 16:39:42
+ * @LastEditTime: 2022-12-04 22:24:33
  */
 import React from 'react'
 // import { createRoot } from 'react-dom/client'
@@ -22,5 +22,5 @@ import React from 'react'
 // const root = createRoot(app!)
 
 // root.render(<React.StrictMode>{rootNode()}</React.StrictMode>)
-const Footer = () => <div>this is Footer</div>
+const Footer = () => <div>this is Footer123123</div>
 export default Footer
